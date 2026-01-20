b87df4a4dcabff17fafe84e88007eaba1f20ee40
Bill <146594930+HopliteRPG@users.noreply.github.com>
2026-01-19 21:47:03 -0500

    for this branch i replace the promise in catOnStartUp with async and await

--- src/randomCat.js
+++ src/randomCat.js
@@ -1,18 +1,13 @@
 export { catOnStartUp };
 function catOnStartUp() {
-  fetch(
-    "https://api.giphy.com/v1/gifs/translate?api_key=G6eaRdpSfKNxkPxUaTfZqlQTunEOpd0y&s=cats",
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
-      const displayImg = document.querySelector(".displayImg");
-      console.log(displayImg);
-      console.log(response.data.images.original.url);
-      displayImg.src = response.data.images.original.url;
-    })
-    .catch((error) => {
-      console.error("Error fetching the image:", error);
-    });
+  const img = document.querySelector(".displayImg");
+
+  async function getCats() {
+    const response = await fetch(
+      "https://api.giphy.com/v1/gifs/translate?api_key=iynhuQUGqRgxLYzfuv3bOxaTvkLzKHeY&s=cats",
+    );
+    const catData = await response.json();
+    img.src = catData.data.images.original.url;
+  }
+  getCats();
 }
